8766a4099dfd30bd028c24b2f6cd35d8b25ce506
Billy Lamberta <billy@lamberta.org>
2010-08-01 14:11:21 -0400

    Reset! Doodle-js v0.1 source archive added to downloads section.

--- src/compat/function.js
+++
@@ -1,9 +0,0 @@
-/* bind(thisObj[, arg...]) */
-if(typeof Function.prototype.bind !== "function") {
-	Function.prototype.bind = function (context) {
-		var fn = this;
-		return function(){
-			return fn.apply(context, arguments);
-		};
-	};
-}
