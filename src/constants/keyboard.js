c128f90d0c9d55329d76f0c0c3e1d821d02a554e
Billy Lamberta <billy@lamberta.org>
2010-08-13 14:55:43 -0400

    Added keyboard constants.

--- src/constants/keyboard.js
+++ src/constants/keyboard.js
@@ -4,6 +4,110 @@ Object.defineProperty(doodle, 'Keyboard', {
   writable: false,
   configurable: false,
   value: Object.create(null, {
+
+    'BACKSPACE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 8
+    },
+    'TAB': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 9
+    },
+    
+    'ENTER': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 13
+    },
+    
+    'COMMAND': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 15
+    },
+    'SHIFT': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 16
+    },
+    'CONTROL': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 17
+    },
+    'ALTERNATE': { //Option key
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 18
+    },
+    'PAUSE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 19
+    },
+    'CAPS_LOCK': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 20
+    },
+    'NUMPAD': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 21
+    },
+    
+    'ESCAPE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 27
+    },
+
+    'SPACE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 32
+    },    
+    'PAGE_UP': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 33
+    },
+    'PAGE_DOWN': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 34
+    },
+    'END': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 35
+    },
+    'HOME': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 36
+    },
+
+    /* ARROWS
+     */ 
     'LEFT': {
       enumerable: true,
       writable: false,
@@ -27,6 +131,520 @@ Object.defineProperty(doodle, 'Keyboard', {
       writable: false,
       configurable: false,
       value: 40
+    },
+
+    /*
+     */
+    'INSERT': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 45
+    },
+    'DELETE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 46
+    },
+
+    /* NUMBERS
+     */
+    'NUMBER_0': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 48
+    },
+    'NUMBER_1': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 49
+    },
+    'NUMBER_2': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 50
+    },
+    'NUMBER_3': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 51
+    },
+    'NUMBER_4': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 52
+    },
+    'NUMBER_5': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 53
+    },
+    'NUMBER_6': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 54
+    },
+    'NUMBER_7': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 55
+    },
+    'NUMBER_8': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 56
+    },
+    'NUMBER_9': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 57
+    },
+
+    /* LETTERS
+     */
+    'A': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 65
+    },
+    'B': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 66
+    },
+    'C': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 67
+    },
+    'D': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 68
+    },
+    'E': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 69
+    },
+    'F': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 70
+    },
+    'G': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 71
+    },
+    'H': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 72
+    },
+    'I': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 73
+    },
+    'J': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 74
+    },
+    'K': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 75
+    },
+    'L': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 76
+    },
+    'M': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 77
+    },
+    'N': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 78
+    },
+    'O': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 79
+    },
+    'P': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 80
+    },
+    'Q': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 81
+    },
+    'R': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 82
+    },
+    'S': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 83
+    },
+    'T': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 84
+    },
+    'U': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 85
+    },
+    'V': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 86
+    },
+    'W': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 87
+    },
+    'X': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 88
+    },
+    'Y': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 89
+    },
+    'Z': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 90
+    },
+
+    'WINDOWS_KEY': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 91
+    },
+
+    /* NUMBER PAD
+     */
+    'NUMPAD_0': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 96
+    },
+    'NUMPAD_1': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 97
+    },
+    'NUMPAD_2': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 98
+    },
+    'NUMPAD_3': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 99
+    },
+    'NUMPAD_4': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 100
+    },
+    'NUMPAD_5': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 101
+    },
+    'NUMPAD_6': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 102
+    },
+    'NUMPAD_7': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 103
+    },
+    'NUMPAD_8': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 104
+    },
+    'NUMPAD_9': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 105
+    },
+    'NUMPAD_MULTIPLY': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 106
+    },
+    'NUMPAD_ADD': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 107
+    },
+    'NUMPAD_ENTER': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 108
+    },
+    'NUMPAD_SUBTRACT': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 109
+    },
+    'NUMPAD_DECIMAL': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 110
+    },
+    'NUMPAD_DIVIDE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 111
+    },
+
+    /* FUNCTION KEYS
+     */
+    'F1': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 112
+    },
+    'F2': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 113
+    },
+    'F3': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 114
+    },
+    'F4': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 115
+    },
+    'F5': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 116
+    },
+    'F6': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 117
+    },
+    'F7': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 118
+    },
+    'F8': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 119
+    },
+    'F9': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 120
+    },
+    'F10': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 121
+    },
+    'F11': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 122
+    },
+    'F12': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 123
+    },
+    'F13': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 124
+    },
+    'F14': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 125
+    },
+    'F15': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 126
+    },
+
+    'SCROLL': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 145
+    },
+
+    /* PUNCTUATION
+     */
+    'SEMICOLON': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 186
+    },
+    'EQUAL': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 187
+    },
+    'COMMA': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 188
+    },
+    'MINUS': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 189
+    },
+    'PERIOD': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 190
+    },
+    'SLASH': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 191
+    },
+    'BACKQUOTE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 192
+    },
+    
+    'LEFTBRACKET': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 219
+    },
+    'BACKSLASH': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 220
+    },
+    'RIGHTBRACKET': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 221
+    },
+    'QUOTE': {
+      enumerable: true,
+      writable: false,
+      configurable: false,
+      value: 222
     }
+    
   })
 });
