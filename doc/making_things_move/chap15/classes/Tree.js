f54257f3881aadcdcc5566fd09d12a63d249ad6a
Billy Lamberta <billy@lamberta.org>
2010-09-03 13:31:56 -0400

    chap15 demos, fireworks and trees.

---
+++ doc/making_things_move/chap15/classes/Tree.js
@@ -0,0 +1,18 @@
+function Tree () {
+  return doodle.Sprite(function () {
+    this.xpos = 0;
+    this.ypos = 0;
+    this.zpos = 0;
+
+    this.graphics.lineStyle(0, 0xffffff);
+    this.graphics.beginPath();
+    this.graphics.lineTo(0, -140 - Math.random() * 20);
+    this.graphics.moveTo(0, -30 - Math.random() * 30);
+    this.graphics.lineTo(Math.random() * 80 - 40,
+                         -100 - Math.random() * 40);
+    this.graphics.moveTo(0, -60 - Math.random() * 40);
+    this.graphics.lineTo(Math.random() * 60 - 30,
+                         -110 - Math.random() * 20);
+    this.graphics.stroke();
+  });
+}
