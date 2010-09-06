f0357cbbda1a7700b0978eeafaf0a57ed653137e
Billy Lamberta <billy@lamberta.org>
2010-09-06 18:30:15 -0400

    Added chap18 demos, matrix math.

---
+++ doc/making_things_move/chap18/classes/Ball3D.js
@@ -0,0 +1,30 @@
+function Ball3D (radius, color) {
+  radius = (radius === undefined) ? 40 : radius;
+  color = (color === undefined) ? "#ff0000" : color;
+  return doodle.Sprite(function () {
+    this.radius = radius;
+    this.xpos = 0;
+    this.ypos = 0;
+    this.zpos = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.vz = 0;
+    this.mass = 1;
+
+    this.graphics.lineStyle(0);
+    this.graphics.beginFill(color);
+    this.graphics.circle(0, 0, this.radius);
+    this.graphics.endFill();
+
+    /* Just a temporary implementation to conform with the book demo.
+     * Redraws circle using a new color value.
+     */
+    this.color_transform = function (colorValue) {
+      this.graphics.clear();
+      this.graphics.lineStyle(0);
+      this.graphics.beginFill(colorValue);
+      this.graphics.circle(0, 0, this.radius);
+      this.graphics.endFill();
+    }
+  });
+}
