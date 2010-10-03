37c59150fbcdfe9147992c8ebc8b1f8fa1fd5731
Billy Lamberta <billy@lamberta.org>
2010-10-03 00:26:33 -0400

    Prettied up api doc style and interaction.

--- doc/api/style/main.js
+++ doc/api/style/main.js
@@ -4,127 +4,192 @@ $(document).ready(function () {
   var mdc_base = "https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/",
       whatwg_base = "http://www.whatwg.org/specs/web-apps/current-work/multipage/",
       doodle_base = "./",
-      //javascript types
-      doc_array = mdc_base+"Array",
-      doc_bool = mdc_base+"Boolean",
-      doc_func = mdc_base+"Function",
-      doc_num = mdc_base+"Number",
-      doc_obj = mdc_base+"Object",
-      doc_string = mdc_base+"String",
-      doc_range_err = mdc_base+"RangeError",
-      doc_ref_err = mdc_base+"ReferenceError",
-      doc_syntax_err = mdc_base+"SyntaxError",
-      doc_type_err = mdc_base+"TypeError",
-      //html5 elements
-      doc_htmlelement = whatwg_base+"elements.html#htmlelement",
-      doc_canvas = whatwg_base+"the-canvas-element.html#the-canvas-element",
-      doc_context = whatwg_base+"the-canvas-element.html#canvasrenderingcontext2d",
-      //doodle types
-      doc_display = doodle_base+"display.html",
-      doc_elemnode = doodle_base+"elementnode.html",
-      doc_evtdisp = doodle_base+"eventdispatcher.html",
-      doc_layer = doodle_base+"layer.html",
-      doc_node = doodle_base+"node.html",
-      doc_sprite = doodle_base+"sprite.html",
-      doc_geom_matrix = doodle_base+"matrix.html",
-      doc_geom_point = doodle_base+"point.html",
-      doc_geom_rect = doodle_base+"rectangle.html",
-      //events
-      doc_evt = doodle_base+"event.html",
-      doc_keyboardevt = doodle_base+"keyboardevent.html",
-      doc_mouseevt = doodle_base+"mouseevent.html",
-      doc_textevt = doodle_base+"textevent.html",
-      doc_touchevt = doodle_base+"touchevent.html",
-      doc_uievt = doodle_base+"uievent.html",
-      //constants
-      doc_gradienttype = doodle_base+"gradienttype.html",
-      doc_keyboard = doodle_base+"keyboard.html"
-      doc_linecap = doodle_base+"linecap.html",
-      doc_linejoin = doodle_base+"linejoin.html",
-      doc_pattern = doodle_base+"pattern.html",
-      //packages
-      doc_utils = doodle_base+"utils.html",
-      doc_utils_types = doodle_base+"utils-types.html";
+      type_docs = {
+        //javascript types
+        'Array': mdc_base+"Array",
+        'Boolean': mdc_base+"Boolean",
+        'Function': mdc_base+"Function",
+        'Number': mdc_base+"Number",
+        'Object': mdc_base+"Object",
+        'String': mdc_base+"String",
+        'Date': mdc_base+"Date",
+        'Error': mdc_base+"Error",
+        'RangeError': mdc_base+"RangeError",
+        'ReferenceError': mdc_base+"ReferenceError",
+        'SyntaxError': mdc_base+"SyntaxError",
+        'TypeError': mdc_base+"TypeError",
+        //html5 elements
+        'HTMLElement': whatwg_base+"elements.html#htmlelement",
+        'HTMLImageElement': whatwg_base+"embedded-content-1.html#the-img-element",
+        'HTMLCanvasElement': whatwg_base+"the-canvas-element.html#the-canvas-element",
+        'CanvasRenderingContext2D': whatwg_base+"the-canvas-element.html#canvasrenderingcontext2d",
+        //doodle types
+        'Display': doodle_base+"display.html",
+        'ElementNode': doodle_base+"elementnode.html",
+        'EventDispatcher': doodle_base+"eventdispatcher.html",
+        'Layer': doodle_base+"layer.html",
+        'Node': doodle_base+"node.html",
+        'Sprite': doodle_base+"sprite.html",
+        'Matrix': doodle_base+"matrix.html",
+        'Point': doodle_base+"point.html",
+        'Rectangle': doodle_base+"rectangle.html",
+        //events
+        'Event': doodle_base+"event.html",
+        'KeyboardEvent': doodle_base+"keyboardevent.html",
+        'MouseEvent': doodle_base+"mouseevent.html",
+        'TextEvent': doodle_base+"textevent.html",
+        'TouchEvent': doodle_base+"touchevent.html",
+        'UIEvent': doodle_base+"uievent.html",
+        //constants
+        'GradientType': doodle_base+"gradienttype.html",
+        'Keyboard': doodle_base+"keyboard.html",
+        'LineCap': doodle_base+"linecap.html",
+        'LineJoin': doodle_base+"linejoin.html",
+        'Pattern': doodle_base+"pattern.html",
+        //packages
+        'utils': doodle_base+"utils.html",
+        'utils.types': doodle_base+"utils-types.html",
+        //libs
+        'Stats': "http://github.com/mrdoob/stats.js"
+      };
 
-  $('body').html(
-    $('body').html()
-      //javascript types
-      .replace(/:Object/g, ":<a href='"+doc_obj+"'>Object</a>")
-      .replace(/:Array/g, ":<a href='"+doc_array+"'>Array</a>")
-      .replace(/:Boolean/g, ":<a href='"+doc_bool+"'>Boolean</a>")
-      .replace(/\|Boolean/g, "|<a href='"+doc_bool+"'>Boolean</a>")
-      .replace(/:Function/g, ":<a href='"+doc_func+"'>Function</a>")
-      .replace(/:Number/g, ":<a href='"+doc_num+"'>Number</a>")
-      .replace(/:String/g, ":<a href='"+doc_string+"'>String</a>")
-      .replace(/\|String/g, "|<a href='"+doc_string+"'>String</a>")
-      //html types
-      .replace(/:HTMLElement/g, ":<a href='"+doc_htmlelement+"'>HTMLElement</a>")
-      .replace(/\|HTMLElement/g, "|<a href='"+doc_htmlelement+"'>HTMLElement</a>")
-      .replace(/:HTMLCanvasElement/g, ":<a href='"+doc_canvas+"'>HTMLCanvasElement</a>")
-      .replace(/:CanvasRenderingContext2D/g, ":<a href='"+doc_context+"'>CanvasRenderingContext2D</a>")
-      //doodle types
-      .replace(/:Display/g, ":<a href='"+doc_display+"'>Display</a>")
-      .replace(/:ElementNode/g, ":<a href='"+doc_elemnode+"'>ElementNode</a>")
-      .replace(/:EventDispatcher/g, ":<a href='"+doc_evtdisp+"'>EventDispatcher</a>")
-      .replace(/:Layer/g, ":<a href='"+doc_layer+"'>Layer</a>")
-      .replace(/:Node/g, ":<a href='"+doc_node+"'>Node</a>")
-      .replace(/:Sprite/g, ":<a href='"+doc_sprite+"'>Sprite</a>")
-      .replace(/:Matrix/g, ":<a href='"+doc_geom_matrix+"'>Matrix</a>")
-      .replace(/:Point/g, ":<a href='"+doc_geom_point+"'>Point</a>")
-      .replace(/:Rectangle/g, ":<a href='"+doc_geom_rect+"'>Rectangle</a>")
-      .replace(/:Event/g, ":<a href='"+doc_evt+"'>Event</a>")
-      .replace(/:KeyboardEvent/g, ":<a href='"+doc_keyboardevt+"'>KeyboardEvent</a>")
-      .replace(/:MouseEvent/g, ":<a href='"+doc_mouseevt+"'>MouseEvent</a>")
-      .replace(/:TextEvent/g, ":<a href='"+doc_textevt+"'>TextEvent</a>")
-      .replace(/:TouchEvent/g, ":<a href='"+doc_touchevt+"'>TouchEvent</a>")
-      .replace(/:UIEvent/g, ":<a href='"+doc_uievt+"'>UIEvent</a>")
-      //doodle classes and packages
-      .replace(/<li>doodle\.utils<\/li>/g, "<li><a href='"+doc_utils+"'>doodle.utils</a><\/li>")
-      .replace(/<li>doodle\.utils\.types<\/li>/g, "<li><a href='"+doc_utils_types+"'>doodle.utils.types</a><\/li>"));
 
-  //replace types in 'Returns' and 'Throws' details section
-  $('details ul li').each(function () {
-    //swap 'Object' first since some of the urls match the phrase
-    $(this).html(
-      $(this).html()
-        .replace(/Object/g, "<a href='"+doc_obj+"'>Object</a>")
-        .replace(/Array/g, "<a href='"+doc_array+"'>Array</a>")
-        .replace(/Boolean/g, "<a href='"+doc_bool+"'>Boolean</a>")
-        .replace(/Function/g, "<a href='"+doc_func+"'>Function</a>")
-        .replace(/Number/g, "<a href='"+doc_num+"'>Number</a>")
-        .replace(/String/g, "<a href='"+doc_string+"'>String</a>")
-        .replace(/RangeError/g, "<a href='"+doc_range_err+"'>RangeError</a>")
-        .replace(/ReferenceError/g, "<a href='"+doc_ref_err+"'>ReferenceError</a>")
-        .replace(/SyntaxError/g, "<a href='"+doc_syntax_err+"'>SyntaxError</a>")
-        .replace(/TypeError/g, "<a href='"+doc_type_err+"'>TypeError</a>")
-        .replace(/HTMLElement/g, "<a href='"+doc_htmlelement+"'>HTMLElement</a>"));
-  });
-
-  //navigation menu links
+  /* Navigation menu items
+   */
   $('nav ul li').each(function () {
-    $(this).html(
-      $(this).html()
-        .replace(/^Display$/g, "<a href='"+doc_display+"'>Display</a>")
-        .replace(/^ElementNode$/g, "<a href='"+doc_elemnode+"'>ElementNode</a>")
-        .replace(/^Event$/g, "<a href='"+doc_evt+"'>Event</a>")
-        .replace(/^EventDispatcher$/g, "<a href='"+doc_evtdisp+"'>EventDispatcher</a>")
-        .replace(/^GradientType$/g, "<a href='"+doc_gradienttype+"'>GradientType</a>")
-        .replace(/^Keyboard$/g, "<a href='"+doc_keyboard+"'>Keyboard</a>")
-        .replace(/^KeyboardEvent$/g, "<a href='"+doc_keyboardevt+"'>KeyboardEvent</a>")
-        .replace(/^Layer$/g, "<a href='"+doc_layer+"'>Layer</a>")
-        .replace(/^LineCap$/g, "<a href='"+doc_linecap+"'>LineCap</a>")
-        .replace(/^LineJoin$/g, "<a href='"+doc_linejoin+"'>LineJoin</a>")
-        .replace(/^Matrix$/g, "<a href='"+doc_geom_matrix+"'>Matrix</a>")
-        .replace(/^MouseEvent$/g, "<a href='"+doc_mouseevt+"'>MouseEvent</a>")
-        .replace(/^Node$/g, "<a href='"+doc_node+"'>Node</a>")
-        .replace(/^Pattern$/g, "<a href='"+doc_pattern+"'>Pattern</a>")
-        .replace(/^Point$/g, "<a href='"+doc_geom_point+"'>Point</a>")
-        .replace(/^Rectangle$/g, "<a href='"+doc_geom_rect+"'>Rectangle</a>")
-        .replace(/^Sprite$/g, "<a href='"+doc_sprite+"'>Sprite</a>")
-        .replace(/^TextEvent$/g, "<a href='"+doc_textevt+"'>TextEvent</a>")
-        .replace(/^TouchEvent$/g, "<a href='"+doc_touchevt+"'>TouchEvent</a>")
-        .replace(/^UIEvent$/g, "<a href='"+doc_uievt+"'>UIEvent</a>")
-        .replace(/^utils$/g, "<a href='"+doc_utils+"'>utils</a>")
-        .replace(/^utils\.types$/g, "<a href='"+doc_utils_types+"'>utils.types</a>"))
+    var item = $(this),
+        txt = item.text(),
+        regexp;
+    for (var type in type_docs) {
+      regexp = new RegExp('^'+type+'$');
+      txt = txt.replace(regexp, "<a href='"+ type_docs[type] +"'>"+ type +"</a>");
+    }
+    item.html(txt);
   });
+
+  /* Description type sig, parameter types
+   */
+  $('.main dt, .main ol li').each(function () {
+    var item = $(this),
+        txt = item.html(),
+        regexp;
+    for (var type in type_docs) {
+      regexp = new RegExp(":"+type, 'g');
+      if (regexp.test(txt)) {
+        txt = txt.replace(regexp, ":<a href='"+ type_docs[type] +"'>"+ type +"</a>");
+      }
+      regexp = new RegExp("\\|"+type, 'g');
+      if (regexp.test(txt)) {
+        txt = txt.replace(regexp, "|<a href='"+ type_docs[type] +"'>"+ type +"</a>");
+      }
+    }
+    item.html(txt);
+  });
+
+  /* Class description, inherititance list
+   * list on single line with arrow between
+   */
+  $("h3:contains('Inheritance') + ol li").each(function (idx) {
+    var item = $(this),
+        txt = item.text(),
+        regexp;
+
+    for (var type in type_docs) {
+      regexp = new RegExp('^'+type+'$');
+      txt = txt.replace(regexp, "<a href='"+ type_docs[type] +"'>"+ type +"</a>");
+    }
+    item.html(txt);
+
+    item.css('display', "inline");
+    if (idx > 0) {
+      item.html("&#8680; " + item.html());
+    }
+  });
+  
+  /* Property items - returns, throws
+   */
+  $('.main ul li').each(function () {
+    var item = $(this),
+        txt = item.text(),
+        regexp;
+    for (var type in type_docs) {
+      regexp = new RegExp('^'+type);
+      txt = txt.replace(regexp, "<a href='"+ type_docs[type] +"'>"+ type +"</a>");
+      regexp = new RegExp("\\|"+type, 'g');
+      if (regexp.test(txt)) {
+        txt = txt.replace(regexp, "|<a href='"+ type_docs[type] +"'>"+ type +"</a>");
+      }
+    }
+    item.html(txt);
+  });
+
+
+  /* Add collapse button
+   */
+  $('.main h1, .main section h2, .main section dt').each(function () {
+    $(this).html($(this).html() + '<span class="expandButton">[-]</span>');
+  });
+
+  /* @return {Boolean} collapse_all
+   */
+  function toggle_expand_button (button) {
+    //toggle character
+    if (button.text() === '[-]') {
+      button.text('[+]');
+      return true;
+    } else {
+      button.text('[-]');
+      return false;
+    }
+  }
+
+  /* Top-level button next to class name.
+   * expand-contract property items
+   */
+  $('.main h1 .expandButton').click(function () {
+    var collapse_all = toggle_expand_button($(this));
+
+    //property descriptions
+    $('.main section dd').each(function () {
+      $(this).animate({
+        'height': collapse_all ? 'hide' : 'show',
+        'opacity': collapse_all ? 'hide' : 'show'
+      }, { duration: "fast" });
+    });
+    //button next to property terms
+    $('.main section dt .expandButton').each(function () {
+      if (collapse_all && $(this).text() === '[+]') {
+        return;
+      } else if (!collapse_all && $(this).text() === '[-]') {
+        return;
+      } else {
+        toggle_expand_button($(this));
+      }
+    });
+  });
+  
+  /* Section collapse button
+   */
+  $('.main section h2 .expandButton').click(function () {
+    var button = $(this);
+    button.parent().siblings('dl').animate({
+      'height': "toggle",
+      'opacity': "toggle"
+    }, { duration: "slow" });
+    toggle_expand_button(button);
+  });
+
+  /* Property item collapse button
+   */
+  $('.main section dt .expandButton').each(function () {
+    var button = $(this);
+    button.click(function () {
+      button.parent().next('dd').animate({
+        'height': "toggle",
+        'opacity': "toggle"
+      }, { duration: "fast" });
+      toggle_expand_button(button);
+    });
+  });
+  
 });
