b05f5eef7592b1de0edd8f8b0ad6cb374cd9b087
Billy Lamberta <b@lamberta.org>
2011-03-23 01:28:07 -0400

    Initial support for doodle.ready().

--- build/doodle-debug.js
+++ build/doodle-debug.js
@@ -11,6 +11,44 @@ doodle.utils = {};
 doodle.geom = {};
 doodle.events = {};
 
+(function () {
+  var ready_queue = [],
+      on_dom_loaded = function () {
+        var queue = ready_queue,
+            len = queue.length,
+            i = 0;
+        if (len > 0) {
+          for (; i < len; i++) {
+            queue[i]();
+          }
+          queue.length = 0;
+        }
+      };
+
+  /**
+   * Pushes a function on the waiting list, will execute when the DOM is ready.
+   * @param {function} fn
+   */
+  doodle.ready = function (fn) {
+    /*DEBUG*/
+    if (typeof fn !== 'function') {
+      console.error("doodle.ready(fn): Parameter must be a function.");
+    }
+    /*END_DEBUG*/
+    ready_queue.push(fn);
+    //already loaded
+    if (document.readyState === "complete") {
+      on_dom_loaded();
+    }
+  };
+
+  if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", on_dom_loaded, false);
+  } else {
+    console.error("document.addEventListener not supported.");
+  }
+}());
+
 /* ES5 compatibility
  */
   
